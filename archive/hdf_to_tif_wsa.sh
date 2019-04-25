3be3cfa5127b3b4425a2b87eb7d7e5c0448a3b15
Arthur Elmes <arthur.elmes@neponset.eeos.umb.edu>
2019-04-25 14:43:17 -0400

    Added downloads folder with batch downloading utilities.

---
+++ archive/hdf_to_tif_wsa.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+in_dir=$1 
+out_dir=$2
+
+if [ ! -d ${out_dir}/wsa/ ]; then
+    mkdir ${out_dir}/wsa/
+fi
+
+if [ ! -d ${out_dir}/wsa/ ]; then
+    mkdir ${out_dir}/wsa/
+fi
+
+if [ ! -d ${out_dir}/bsa/ ]; then
+    mkdir ${out_dir}/bsa/
+fi
+
+for hdf in $in_dir/*.hdf
+do
+    echo $hdf
+    filename=$(basename -- $hdf)
+    extension="${filename##*.}"
+    filename_bare="${filename%.*}"
+    # gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b01_1 ${out_dir}/${filename_bare}_sr_b1.tif
+    # gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b02_1 ${out_dir}/${filename_bare}_sr_b2.tif
+    # gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b03_1 ${out_dir}/${filename_bare}_sr_b3.tif
+    # gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b04_1 ${out_dir}/${filename_bare}_sr_b4.tif
+    # gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b05_1 ${out_dir}/${filename_bare}_sr_b5.tif
+    # gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b06_1 ${out_dir}/${filename_bare}_sr_b6.tif
+    # gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:sur_refl_b07_1 ${out_dir}/${filename_bare}_sr_b7.tif
+    # gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MODIS_Grid_500m_2D:QC_500m_1 ${out_dir}/${filename_bare}_sr_qc.tif
+    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:Albedo_WSA_shortwave ${out_dir}/wsa/${filename_bare}_wsa_shortwave.tif
+    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:Albedo_BSA_shortwave ${out_dir}/bsa/${filename_bare}_bsa_shortwave.tif
+    gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:BRDF_Albedo_Band_Mandatory_Quality_shortwave ${out_dir}/qa/${filename_bare}_qa_shortwave.tif
+done
---
+++ download/mod11a2/feed_download_mod11a2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#TODO add args for which prduct number and tile you want and then change corresponding download.sh to intake the tile number
+
+# Pass in the yyyy as arg1
+start_date=$1 #2014-01-01 #${1}-01-01
+end_date=$2 #2014-12-31 #$(date -I -d "$start_date+1 year")
+tile_id=$3
+
+cur_date=$start_date
+
+url_base=https://e4ftl01.cr.usgs.gov/MOLT/MOD11A2
+vers=.006
+
+dl_dir=/muddy/data01/arthur.elmes/above/${start_date:0:4}/mod11a2/${tile_id}
+
+if [ ! -d ${dl_dir} ]; then
+    mkdir ${dl_dir}
+fi
+
+#echo $dl_dir
+#echo $start_date
+#echo $end_date
+
+# Loop through all D products from 1 to 21
+# Each band has the three params: geo, iso, vol
+# Loop through all dates in year, pass to download script with full url
+while [[ "$cur_date" < "$end_date" ]]; do
+    cur_date_url=${cur_date:0:4}.${cur_date:5:2}.${cur_date:8:2}
+    dl_url=${url_base}${i}${vers}/${cur_date_url}
+    cur_date=$(date -I -d "$cur_date+1 day")
+    echo $dl_url
+    if [ ! -r $dl_dir ]; then
+	mkdir -p $dl_dir
+    fi
+    bash ./download.sh $dl_url $dl_dir $tile_id
+done
+# cur_date=$start_date
+ 
+
+
+
+
+# call downloader script with 2 args: url and dl_dir
+#bash ./download.sh $url .
+#echo 
