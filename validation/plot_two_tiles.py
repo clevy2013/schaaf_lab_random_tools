dabbbccad261f4b540548dd11b80233d520576c4
Arthur <arthur.elmes@gmail.com>
2020-02-10 11:04:01 -0500

    plot_two_tiles.py working for MCD, now add VNP functionality with manual tile extents for golden tiles.

--- validation/plot_two_tiles.py
+++ validation/plot_two_tiles.py
@@ -1,15 +1,28 @@
 #!/home/arthur.elmes/software/anaconda3/envs/geo/bin/python
 
-import os, glob, matplotlib, math, sys
+import os, matplotlib, math, sys
 import matplotlib.pyplot as plt
-from matplotlib import cm
+#from matplotlib import cm
 from sklearn.metrics import mean_squared_error
 import numpy as np
-import pandas as pd
-import rasterio as rio
-from pyhdf.SD import SD, SDC 
+#import pandas as pd
+#import rasterio as rio
+from pyhdf.SD import SD, SDC
+import h5py
 
 matplotlib.rcParams['agg.path.chunksize'] = 100000
+
+## h12v04 UL: -6671703.1179999997839332 5559752.5983330002054572 LR: -5559752.5983330002054572 4447802.0786669999361038
+## h16v01 UL: -2223901.0393329998478293 8895604.1573329996317625 LR: -1111950.5196670000441372 7783653.6376670002937317
+## h16v02 UL: -2223901.0393329998478293 7783653.6376670002937317 LR: -1111950.5196670000441372 6671703.1179999997839332
+
+def modis_process():
+    #TODO fill in the processing chain for modis hdf products here, calling hdf_to_np etc
+    pass
+
+def viirs_process():
+    #TODO mirror the modis processing chain that's already laid out and working, but with h5py
+    pass
 
 def hdf_to_np(hdf_fname, sds):
     hdf_ds = SD(hdf_fname, SDC.READ)
@@ -28,13 +41,13 @@ def plot_data(cmb_data, labels, stats, workspace):
     # Using masked numpy arrays, create scatterplot of tile1 vs tile2
     plt.ion()
     fig = plt.figure()
-    fig.suptitle(labels[1] + ' vs ' + labels[2])
+    fig.suptitle(labels[0] + '_' + labels[1])
     ax = fig.add_subplot(111)
     #fig.subplots_adjust(top=0.85)
 
-    ax.set_title('White Sky Albedo Comparison')
-    ax.set_xlabel(labels[1] + ' (scaled)')
-    ax.set_ylabel(labels[2] + ' (scaled)')
+    ax.set_title(labels[3])
+    ax.set_xlabel(labels[1] + " " + labels[4] + ' (scaled)')
+    ax.set_ylabel(labels[2] + " " + labels[5] + ' (scaled)')
     plt.xlim(0.0, 1000)
     plt.ylim(0.0, 1000)
 
@@ -46,7 +59,9 @@ def plot_data(cmb_data, labels, stats, workspace):
     props = dict(boxstyle='round', facecolor='white', alpha=0.5)
     ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14, verticalalignment='top', bbox=props)
     ax.plot(cmb_data[:, 0], cmb_data[:, 1], marker=',', color='b', linestyle="None")
-    plt_name = os.path.join(workspace, labels[0]) #str(year + '_' + series_name.replace(" ", ""))
+    #TODO add band name to this plot output name
+    plt_name = os.path.join(workspace, labels[0] + "_" + labels[1] + "_" + labels[4] + "_vs_" \
+                            + labels[2] + "_" + labels[5])
 
     # Add x=y line
     lims = [
@@ -74,34 +89,55 @@ def plot_data(cmb_data, labels, stats, workspace):
     #plt.savefig('{plt_name}_heatmap.png'.format(plt_name=plt_name))
 
     # Export data as CSV in case needed
-    np.savetxt(labels[1] + "_" + labels[2] + 'test_data.csv', cmb_data, delimiter=",")
+    hdrs = str(labels[1] + "." + labels[4] + "," + labels[2] + "." + labels[5])
+    np.savetxt(labels[0] + "_" + labels[1] + "_" + labels[2] + '_test_data.csv', cmb_data, delimiter=",", header=hdrs)
 
 def main():
     # Set workspace IO dirs
-    workspace = "/muddy/data05/arthur.elmes/MCD43/MCD43A3/h12v04/"
-    # workspace = sys.argv[1]
-
+    workspace = sys.argv[1] #"/muddy/data05/arthur.elmes/MCD43/MCD43A3/h12v04/"
+    workspace_out = workspace 
     os.chdir(workspace)
 
-    # tile1_fname_wsa = sys.argv[2]
-    # tile2_fname_wsa = sys.argv[3]
-
-    # These probably won't ever change, but maybe they should be args so
-    # that different bands can be selected
-    sds_name_wsa = "Albedo_WSA_shortwave"
-    sds_name_qa = "BRDF_Albedo_Band_Mandatory_Quality_shortwave"
-
+    # test tile 1 filename: MCD43A3.A2016365.h12v04.006.2017014043109.hdf
+    # test tile 2 filename: MCD43A3.A2016366.h12v04.006.2017014050856.hdf 
+    
+    #TODO these shouldn't just be wsa, should be able to do
+    # any band, selected as argument they should be args so
+    # that different bands can be selected. Right now these are manually set
+    # to reflect the band of interest
+    
+    # sds_name_wsa = "Albedo_WSA_shortwave"
+    # sds_name_qa = "BRDF_Albedo_Band_Mandatory_Quality_shortwave"
+    sds_name_wsa = "Albedo_WSA_nir"
+    sds_name_qa = "BRDF_Albedo_Band_Mandatory_Quality_nir"
+    
     # Set input hdf/h5 filenames
     #TODO replace hard coded with cl args
-    tile1_fname = os.path.join(workspace,"MCD43A3.A2016366.h12v04.006.2017014050856.hdf")
-    tile2_fname = os.path.join(workspace, "MCD43A3.A2016365.h12v04.006.2017014043109.hdf")
+    tile1_fname =  sys.argv[2] #TODO maybe later add an os.path.join with the workspace here? but maybe not
+    tile2_fname =  sys.argv[3]
 
     # Extract identifying information from filenames
-    tile = tile1_fname.split("/")[-1][17:23]
-    short_name_tile1 = tile1_fname.split("/")[-1][:16]
-    short_name_tile2 = tile2_fname.split("/")[-1][:16]
-    labels = (tile, short_name_tile1, short_name_tile2)
+    sensor = tile1_fname.split("/")[-1][:3]
     
+    # Check that sensor is sensible
+    if ( sensor == "MCD" ):
+        tile = tile1_fname.split("/")[-1][17:23]
+        short_name_tile1 = tile1_fname.split("/")[-1][:16]
+        short_name_tile2 = tile2_fname.split("/")[-1][:16]
+        collection_tile1 = tile1_fname.split("/")[-1][24:27]
+        collection_tile2 = tile2_fname.split("/")[-1][24:27]
+    elif ( sensor == "VNP" ):
+        tile = tile1_fname.split("/")[-1][18:24]
+        short_name_tile1 = tile1_fname.split("/")[-1][:17]
+        short_name_tile2 = tile2_fname.split("/")[-1][:17]
+        collection_tile1 = tile1_fname.split("/")[-1][25:28]
+        collection_tile2 = tile2_fname.split("/")[-1][25:28]
+    elif ( sensor != "MCD" and sensor != "VNP"):
+        print("Check input data type!")
+        sys.exit()
+
+    labels = (tile, short_name_tile1, short_name_tile2, sds_name_wsa, collection_tile1, collection_tile2)
+
     # Convert both tiles' data and qa to numpy arrays for plotting
     tile1_data_wsa = hdf_to_np(tile1_fname, sds_name_wsa)
     tile2_data_wsa = hdf_to_np(tile2_fname, sds_name_wsa)
